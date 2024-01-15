74bf7d83047a98c2173b00042b631a96134b7cec
Vivek Singh Mehta <viveksinghmehta.smr@gmail.com>
2024-01-15 22:35:56 +0530

    First commit

---
+++ Face Facts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Face Facts
+//
+//  Created by Vivek Singh Mehta on 15/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Face Facts/Face_FactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Face_FactsApp.swift
+//  Face Facts
+//
+//  Created by Vivek Singh Mehta on 15/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Face_FactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
